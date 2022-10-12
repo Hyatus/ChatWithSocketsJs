b46bfdc0afb3645d428e551015f4c16bf80df787
Hyatus <2831873010101@ingenieria.usac.edu.gt>
2022-10-12 00:07:57 -0600

    estableciendo protocolo para cerrar la conexión

--- client.js
+++ client.js
@@ -1,11 +1,20 @@
 const { Socket } = require("net");
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+}); // Leer mensajes desde la consola
 
 const socket = new Socket();
 
-socket.connect({host:'localhost',port: 8000});
+socket.connect({ host: "localhost", port: 8000 });
 
-socket.write('Hola'); // Envía un Buffer datos en binario 
-socket.setEncoding('utf-8'); // Convierte la data en binario a texto
-socket.on('data',(data)=>{
-    console.log(data);
-})
+// Cuando el usuario escriba una línea es mandarsela al servidor
+readline.on("line", (line) => {
+  socket.write(line);
+});
+
+// socket.write("Hola"); // Envía un Buffer datos en binario
+socket.setEncoding("utf-8"); // Convierte la data en binario a texto
+socket.on("data", (data) => {
+  console.log(data);
+});
--- server.js
+++ server.js
@@ -2,19 +2,28 @@ const { Server } = require("net");
 
 const server = new Server();
 
-server.on("connection", (socket) => {
-  console.log(
-    `New connection from ${socket.remoteAddress}:${socket.remotePort}`
-  );
+// Vamos a definir una cadena que indique cuando se ha acabao la conexión
+const END = 'END';
 
-  socket.setEncoding('utf-8'); // Convierte la data en binario a texto 
+server.on("connection", (socket) => {
+  const remoteSocket = `${socket.remoteAddress}:${socket.remotePort} `;
+  console.log("New connection from: ", remoteSocket);
+
+  socket.setEncoding("utf-8"); // Convierte la data en binario a texto
 
   // Cuando nos envíe datos el cliente
-  socket.on("data", (data) => {
-    socket.write(data); // Devolvemos la información que nos envío el cliente
+  socket.on("data", (message) => {
+
+    if (message === END){
+        console.log(`Connection with ${remoteSocket} closed`)
+        socket.end(); // Se cierra el socket y se liberan los recursos que estaba utilizando
+    }else{
+        console.log(`${remoteSocket} -> ${message}`);
+    }
+    
+    //socket.write(data); // Devolvemos la información que nos envío el cliente
   });
 });
-
 
 server.listen({ port: 8000, host: "0.0.0.0" }, () => {
   console.log("listening on port 8000");
