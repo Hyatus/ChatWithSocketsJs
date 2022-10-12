651347f284d7f72f9aeadea6dda14af4c417efa9
Hyatus <2831873010101@ingenieria.usac.edu.gt>
2022-10-12 00:35:07 -0600

    - configurando nuestros puertos

--- client.js
+++ client.js
@@ -4,13 +4,19 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 }); // Leer mensajes desde la consola
 
+const END = "END";
+
 const socket = new Socket();
+
 
 socket.connect({ host: "localhost", port: 8000 });
 
 // Cuando el usuario escriba una línea es mandarsela al servidor
-readline.on("line", (line) => {
-  socket.write(line);
+readline.on("line", (message) => {
+  socket.write(message);
+  if (message === END) {
+    socket.end();
+  }
 });
 
 // socket.write("Hola"); // Envía un Buffer datos en binario
@@ -18,3 +24,6 @@ socket.setEncoding("utf-8"); // Convierte la data en binario a texto
 socket.on("data", (data) => {
   console.log(data);
 });
+
+
+socket.on("close",()=>process.exit(0)); // Esperamos a que el servidor nos confirme la finalización del proceso, entonces lo matamos
--- server.js
+++ server.js
@@ -1,30 +1,61 @@
 const { Server } = require("net");
 
-const server = new Server();
+// const server = new Server(); -> Dejo de ser un variable global
 
 // Vamos a definir una cadena que indique cuando se ha acabao la conexión
-const END = 'END';
+const END = "END";
+const host = '0.0.0.0';
 
-server.on("connection", (socket) => {
-  const remoteSocket = `${socket.remoteAddress}:${socket.remotePort} `;
-  console.log("New connection from: ", remoteSocket);
+const listen = (port) => {
 
-  socket.setEncoding("utf-8"); // Convierte la data en binario a texto
-
-  // Cuando nos envíe datos el cliente
-  socket.on("data", (message) => {
-
-    if (message === END){
-        console.log(`Connection with ${remoteSocket} closed`)
+  const server = new Server();
+  server.on("connection", (socket) => {
+    const remoteSocket = `${socket.remoteAddress}:${socket.remotePort} `;
+    console.log("New connection from: ", remoteSocket);
+    socket.setEncoding("utf-8"); // Convierte la data en binario a texto
+    // Cuando nos envíe datos el cliente
+    socket.on("data", (message) => {
+      if (message === END) {
         socket.end(); // Se cierra el socket y se liberan los recursos que estaba utilizando
-    }else{
+      } else {
         console.log(`${remoteSocket} -> ${message}`);
-    }
-    
-    //socket.write(data); // Devolvemos la información que nos envío el cliente
+      }
+      //socket.write(data); // Devolvemos la información que nos envío el cliente
+    });
+    server.listen({ port, host}, () => {
+      console.log(`listening on port: ${port}`);
+    });
+    socket.on("close", () => {
+      console.log(`Connection with ${remoteSocket} closed`);
+    });
   });
-});
 
-server.listen({ port: 8000, host: "0.0.0.0" }, () => {
-  console.log("listening on port 8000");
-});
+};
+
+const error = (err) => {
+  console.error(err);
+  process.exit(1);
+};
+
+const main = () => {
+  // Queremos recibir un puerto por la consola
+  if (process.argv.length !== 3) {
+    error(`Usage: node ${__filename} port`);
+  }
+  // console.log(process.argv); // Lista de argumentos que nos van a dar
+
+  let port = process.argv[2]; // Tomamos el puerto
+  if (isNaN(port)) {
+    // Si el puerto no es un número
+    error(`Invalid port: ${port} `);
+  }
+
+  port = Number(port);
+
+  listen(port);
+};
+
+// Si este es el archivo main ejecuta main
+if (require.main === module) {
+  main();
+}
